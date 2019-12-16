c03b062923591e18c1bf7db4d9569def9e90b95b
Anal Kumar <analkumar2@gmail.com>
2019-12-16 17:48:54 +0530

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='brute_curvefit',
-    version='1.0.0',
-    scripts=['brute_curvefit.py'],
+    version='0.0.1',
     author="Anal Kumar",
     author_email="analkumar2@gmail.com",
     description="Fits a given function with any number of parameters to a given array of values using brute force as well as scipy optimize. Then returns the parameters.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/analkumar2/brute_curvefit",
-    packages=setuptools.find_packages(),
+    package_dir={'brute_curvefit': '.'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
